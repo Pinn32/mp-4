da36ccbbd583b0be7841c3bc4bc7e76e343b8ddb
Pinn Xu <32sanliang32@gmail.com>
2026-03-24 23:51:18 -0400

    fix fetch

--- app/api/getWeatherData/route.ts
+++ app/api/getWeatherData/route.ts
@@ -1,14 +1,18 @@
-export async function GET(): Promise<Response> {
-    const API_KEY = process.env.API_KEY;
+import { NextResponse } from "next/server";
 
-    // Make API request
-    const res = await fetch(`https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`, {
-        cache: "no-store",
-    });
+export const dynamic = "force-dynamic";
 
-    // Parse JSON data from API Response
+const API_KEY = process.env.API_KEY;
+
+export async function GET(): Promise<NextResponse> {
+    const res = await fetch(
+        `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
+    );
+
+    if (res.status !== 200) {
+        return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+    }
+
     const data = await res.json();
-
-    // Return the parsed JSON data
-    return Response.json(data);  // Response: Web API, HTTP Response
+    return NextResponse.json(data);
 }
